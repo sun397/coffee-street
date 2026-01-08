779f469ae0ad4ef301880dbcc2d5256c7be88b7a
BTM\d-ogata <d-ogata@b-tm.co.jp>
2026-01-08 17:26:44 +0900

    feat: initialize shadcn/ui

---
+++ packages/ui/src/index.ts
@@ -0,0 +1,6 @@
+export * from "./components/ui/badge";
+export * from "./components/ui/button";
+export * from "./components/ui/card";
+export * from "./components/ui/label";
+export * from "./lib/utils";
+
